8a1b1b75b5d1e13be5d6c6bec97615d05eb6d303
Rivsen Tan <rivsen1003@gmail.com>
2016-07-28 00:46:55 +0800

    save work

--- kernel.js
+++ kernel.js
@@ -19,31 +19,33 @@
     );
   };
 
-  var rswork = {};
-
-  function App(name) {
-    this.name = name;
-  }
-
-  App.prototype = {
-    constructor: App,
-    name: 'Base App',
-    init: function () {
-      console.log('init', this);
-    },
-    boot: function () {
-      console.log('boot', this);
-    }
+  module.inheritPrototype = function (subType, superType) {
+    var prototype = Object(superType.prototype);
+    prototype.constructor = subType;
+    subType.prototype = prototype;
   };
 
+  var rswork = {};
+
   function Request() {
-    console.log('request created');
+    console.log('request created'); 
   }
 
   Request.fn = Request.prototype = {
     constructor: Request,
+    kernel: null,
     createFromGlobal: function () {
       return new Request();
+    },
+    copy: function() {
+      return new Request();
+    },
+    setKernel: function(kernel) {
+      if (kernel instanceof Kernel) {
+        this.kernel = kernel;
+      }
+
+      return this;
     }
   };
 
@@ -53,19 +55,79 @@
 
   Response.fn = Response.prototype = {
     constructor: Response,
+    kernel: null,
+    sended: false,
     send: function () {
-      console.log('responsed');
+      console.log('send' + this.kernel.id, this.sended);
+      this.sended = true;
+
+      var info = document.createElement('p');
+      info.innerHTML = this.kernel.name + ' responsed';
+
+      this.kernel.rootElement.appendChild(info);
+
+      console.log(this.kernel.id + 'responsed');
+    },
+    setKernel: function(kernel) {
+      if (kernel instanceof Kernel) {
+        this.kernel = kernel;
+      }
+
+      return this;
     }
   };
 
   function NotFoundResponse() {
+    Response.call(this);
+
     this.send = function() {
       console.log('404 responsed');
+
+      this.prototype.send.call(this);
     };
   }
 
-  NotFoundResponse.prototype = new Response();
-  NotFoundResponse.prototype.constructor = NotFoundResponse;
+  module.inheritPrototype(NotFoundResponse, Response);
+
+  function childrenKernel() {
+    var children = {};
+
+    this.get = function (id) {
+      if (children[id] instanceof Kernel) {
+        return children[id];
+      }
+
+      return undefined;
+    };
+
+    this.append = function (kernel) {
+      if (!(kernel instanceof Kernel)) {
+        throw "not a kernel object";
+      }
+
+      children[kernel.id] = kernel;
+
+      return this;
+    }
+
+    this.remove = function (id) {
+      if (children[id] instanceof Kernel) {
+        return children[id];
+      }
+
+      return undefined;
+    }
+
+    this.all = function () {
+      var tmpChildren = {};
+
+      for (id in children) {
+        tmpChildren[id] = children[id];
+      }
+
+      return tmpChildren;
+    }
+  }
 
   function Kernel(id, rootElement, env, debug) {
     if (!module.isElement(rootElement)) {
@@ -77,7 +139,8 @@
       id: id,
       rootElement: rootElement,
       env: env,
-      debug: debug
+      debug: debug,
+      children: new childrenKernel()
     };
 
     // readonly property
@@ -91,6 +154,13 @@
     }
 
     this.name =  module.ucfirst(id) + ' Kernel';
+    this.appendChild = function(childKernel) {
+      this.children.append(childKernel);
+
+      return this;
+    }
+
+    console.log(this.name+' created');
   }
 
   Kernel.prototype = {
@@ -102,10 +172,15 @@
     debug: true, 
     handle: function(request) {
       if (!(request instanceof Request)) {
-        return new NotFoundResponse();
+        var response = new NotFoundResponse();
+      } else {
+        request.setKernel(this);
+        var response = new Response();
       }
 
-      return new Response();
+      response.setKernel(this);
+
+      return response;
     }
   };
 
